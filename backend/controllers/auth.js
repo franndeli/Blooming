39a19d0892f062b840af4f79a70ae354b33d8501
frandeli <fjdg5@alu.ua.es>
2023-12-09 17:37:47 +0100

    Fix problema de Rol en el login (te odio iker)

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,59 +1,44 @@
 const { dbConnection } = require('../database/configdb');
 const connection = dbConnection();
 const bcrypt = require('bcryptjs');
-
 const { generarJWT } = require('../helpers/jwt');
 
 const login = async (req, res) => {
     const { Usuario, Contraseña } = req.body;
 
     try {
-        // Verifica primero en la tabla de alumnos
-        connection.query('SELECT *, "Alumno" as Rol FROM alumno WHERE Usuario = ?', [Usuario], async (error, results) => {
-            if (error) {
-                console.log(error);
-                return res.status(500).json({
-                    ok: false,
-                    msg: 'Error en login',
-                    token: ''
-                });
-            }
+        const usuario = await buscarUsuario(Usuario);
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario o contraseña incorrectos',
+                token: ''
+            });
+        }
 
-            if (results.length > 0) {
-                // Usuario encontrado en la tabla de alumnos
-                return procesarLogin(results[0], Contraseña, res);
-            } else {
-                // Si no es alumno, verificar si es profesor
-                connection.query('SELECT *, "Profesor" as Rol FROM profesor WHERE Email = ?', [Usuario], async (error, results) => {
-                    if (results.length > 0) {
-                        // Usuario encontrado en la tabla de profesores
-                        return procesarLogin(results[0], Contraseña, res);
-                    } else {
-                        // Si no es profesor, verificar si es centro escolar
-                        connection.query('SELECT *, "Centro" as Rol FROM centro_escolar WHERE Email = ?', [Usuario], async (error, results) => {
-                            if (results.length > 0) {
-                                // Usuario encontrado en la tabla de centros escolares
-                                return procesarLogin(results[0], Contraseña, res);
-                            } else {
-                                connection.query('SELECT *, "Admin" as Rol FROM admin WHERE Email = ?', [Usuario], async (error, results) => {
-                                    if (results.length > 0) {
-                                        // Usuario encontrado en la tabla de centros escolares
-                                        return procesarLogin(results[0], Contraseña, res);
-                                    } else {
-                                        
-                                        // Usuario no encontrado
-                                        return res.status(400).json({
-                                            ok: false,
-                                            msg: 'No existe en ninguna tabla',
-                                            token: ''
-                                        });
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+        // Comprobar si el rol almacenado coincide con el esperado
+        if (usuario.Rol !== usuario.RolEsperado) {
+            return res.status(400).json({
+                ok: false,
+                msg: `Rol no autorizado: se esperaba ${usuario.RolEsperado}`,
+                token: ''
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(Contraseña, usuario.Contraseña);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario o contraseña incorrectos',
+                token: ''
+            });
+        }
+
+        const token = await generarJWT(usuario.ID_Alumno || usuario.ID_Profesor || usuario.ID_Centro, usuario.Rol);
+        res.json({
+            ok: true,
+            msg: `login ${usuario.Rol}`,
+            token
         });
     } catch (error) {
         console.log(error);
@@ -65,22 +50,39 @@ const login = async (req, res) => {
     }
 };
 
-const procesarLogin = async (usuario, contraseña, res) => {
-    const validPassword = bcrypt.compareSync(contraseña, usuario.Contraseña);
-    if (!validPassword) {
-        return res.status(400).json({
-            ok: false,
-            msg: 'Usuario o contraseña incorrectos una vez validado',
-            token: '',
-        });
-    } else {
-        const token = await generarJWT(usuario.ID_Alumno || usuario.ID_Profesor || usuario.ID_Centro, usuario.Rol);
-        res.json({
-            ok: true,
-            msg: `login ${usuario.Rol}`,
-            token
-        });
-    }
-};
+async function buscarUsuario(usuario) {
+    const tablas = [
+        { nombre: 'alumno', campoUsuario: 'Usuario', rolEsperado: 'Alumno' },
+        { nombre: 'profesor', campoUsuario: 'Email', rolEsperado: 'Profesor' },
+        { nombre: 'centro_escolar', campoUsuario: 'Email', rolEsperado: 'Centro' },
+        { nombre: 'admin', campoUsuario: 'Email', rolEsperado: 'Admin' }
+    ];
 
-module.exports = { login }
+    for (const tabla of tablas) {
+        const query = `SELECT *, ? as RolEsperado FROM ${tabla.nombre} WHERE ${tabla.campoUsuario} = ?`;
+        try {
+            const results = await consultaAsync(query, [tabla.rolEsperado, usuario]);
+            if (results && results.length > 0) {
+                return results[0];
+            }
+        } catch (error) {
+            console.error(`Error en la consulta a la tabla ${tabla.nombre}:`, error);
+        }
+    }
+    return null;
+}
+
+function consultaAsync(query, params) {
+    return new Promise((resolve, reject) => {
+        connection.query(query, params, (error, results, fields) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
+
+
+module.exports = { login };

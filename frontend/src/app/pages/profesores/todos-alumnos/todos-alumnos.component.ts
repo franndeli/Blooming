9b11b03a0113152e5b4d9ba2af028e37bb292a95
cgs139 <cgs139@alu.ua.es>
2024-05-21 01:01:32 +0200

    eyyo

--- frontend/src/app/pages/profesores/todos-alumnos/todos-alumnos.component.ts
+++ frontend/src/app/pages/profesores/todos-alumnos/todos-alumnos.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlumnoService } from '../../../services/alumnos.service';
 import { ProfesorService } from '../../../services/profesores.service';
+import { RespuestaService } from '../../../services/respuestas.service';
+import { SesionService } from '../../../services/sesiones.service';
 
 
 @Component({
@@ -14,15 +16,19 @@ export class TodosAlumnosComponent {
   private idClase: any;
   private idCentro: any;
   alumnosData: any;
+  public alumnosTendencia: any;
+  sesionAlumnoAnterior: any;
   filtroNombre: string = ''; 
   public posActual = 0;
   public filPag = 9;
   private busqueda = '';
   private contar = 0;
   public totalAlumnos = 0;
+  private EstadoAnterior: any;
 
-  constructor(private alumnoService: AlumnoService, private router: Router, private profesorService: ProfesorService){
+  constructor(private alumnoService: AlumnoService, private router: Router, private sesionService: SesionService, private profesorService: ProfesorService, private respuestaService: RespuestaService){
     this.alumnosData = [];
+    this.alumnosTendencia = [];
   }
 
   ngOnInit(){
@@ -50,11 +56,14 @@ export class TodosAlumnosComponent {
           this.getAlumnosCentro(this.busqueda);
         }else {
           this.alumnosData = [];
+          this.alumnosTendencia = [];
           this.totalAlumnos = 0;
         }
       }else{
         this.alumnosData = res.alumnos;
+        this.alumnosTendencia = [];
         this.totalAlumnos = res.page.total;
+        this.compararEstados();
       }
     });
   }
@@ -119,4 +128,58 @@ export class TodosAlumnosComponent {
 
     return clase;
   }
+
+  compararEstados() {
+    this.EstadoAnterior = '';
+    const estados = ['Muy Malo', 'Malo', 'Normal', 'Bueno', 'Muy Bueno']; // Orden de los estados de peor a mejor
+
+    for (let i = 0; i < this.totalAlumnos; i++) {
+      this.sesionService.getSesionesAlumno(this.alumnosData[i].ID_Alumno, 7).subscribe((res: any) => {
+        console.log(res);
+        if(res.sesiones.length > 0){
+          this.sesionAlumnoAnterior = res.sesiones[0];
+          console.log(this.sesionAlumnoAnterior);
+
+          let data = JSON.parse(this.sesionAlumnoAnterior.ValorAmbitoFin);
+
+          let values: number[] = Object.values(data) as number[];
+          let sum = values.reduce((a, b) => a + b, 0);
+          let average = sum / values.length;
+  
+          console.log('La media es: ', average);
+
+          if(average >= 0 && average <= 20){
+            this.EstadoAnterior = 'Muy Malo';
+          } else if (average > 20 && average <= 40){
+            this.EstadoAnterior = 'Malo';
+          } else if (average > 40 && average <= 60){
+            this.EstadoAnterior = 'Normal';
+          } else if (average > 60 && average <= 80){
+            this.EstadoAnterior = 'Bueno';
+          } else if (average > 80 && average <= 100){
+            this.EstadoAnterior = 'Muy Bueno';
+          }
+          console.log('El estado anterior es: ', this.EstadoAnterior);
+          console.log('El estado actual es: ', this.alumnosData[i].Estado);
+
+          // Comparación de estados
+          const indiceEstadoAnterior = estados.indexOf(this.EstadoAnterior);
+          const indiceEstadoActual = estados.indexOf(this.alumnosData[i].Estado);
+
+          if (indiceEstadoActual > indiceEstadoAnterior) {
+            this.alumnosTendencia.push({id: this.alumnosData[i].ID_Alumno, tendencia: 'mejora'});
+          } else if (indiceEstadoActual < indiceEstadoAnterior) {
+            this.alumnosTendencia.push({id: this.alumnosData[i].ID_Alumno, tendencia: 'empeora'});
+          } else {
+            this.alumnosTendencia.push({id: this.alumnosData[i].ID_Alumno, tendencia: 'igual'});
+          }
+          
+        } else{
+          this.alumnosTendencia.push({id: this.alumnosData[i].ID_Alumno, tendencia: 'igual'});
+        }
+      });
+    }
+    console.log('La tendencia es: ', this.alumnosTendencia);
+  }
+  
 }
